fd373b2f7c907dab752967320f2a2c931d5b1804
SoloGudLyf <solomonteiko2006@gmail.com>
2025-11-07 22:36:33 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const homeBtn = document.querySelector("#home");
+homeBtn.addEventListener("click", pageLoad);
+
+const menuBtn = document.querySelector("#menu");
+menuBtn.addEventListener("click", menuTab);
+
+const contact = document.querySelector("#contact");
+contact.addEventListener("click", contactUs);
